ea8a72e46c2478e7d00c691986709235919375f8
Jesus <jesus@winterwind.com>
2019-06-24 16:06:30 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # zendx-dev
+
+
+http://zendx.test/index/details/amazing/grace/love/me?test=1&test1=2
+
+result: 
+
+Array
+(
+    [controller] => index
+    [action] => details
+    [amazing] => grace
+    [love] => me
+    [module] => default
+    [test] => 1
+    [test1] => 2
+)
---
+++ application/controllers/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+        return "test";
+    }
+
+    public function detailsAction()
+    {
+        $params = $this->getAllParams();
+
+        echo "<pre>";
+        print_r($params);
+        echo "</pre>";
+
+        $this->view->assign([
+            'name'=> 'Jesus',
+            'surename' => 'Suarez'
+        ]);
+
+        $this->_helper->json($params);
+    }
+}
+
